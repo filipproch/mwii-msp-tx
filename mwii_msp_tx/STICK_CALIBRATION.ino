ec00ae2d6c9f264ad55b6a2f02ebd0122ecdd25c
andrejj <andrejj@gmail.com>
2012-11-02 16:27:01 +0000

    Working. Stil without status feedback.

--- mwii_msp_tx/STICK_CALIBRATION.ino
+++ mwii_msp_tx/STICK_CALIBRATION.ino
@@ -2,28 +2,29 @@
 void stickcal(){
   uint32_t caltime=millis();
   uint16_t calv=0;
-  
-  
+
+
   analogWrite(BUZERPIN,50);
 
   while((millis()-caltime)<10000){
-    
+
     for(int u=0;u<4;u++){
       calv=analogRead(u+1);
-      
+
       if(calv>ANLOGMINMAX[u][1]){
         ANLOGMINMAX[u][1]=calv;
       }
-      
+
       if(calv<ANLOGMINMAX[u][0]){
         ANLOGMINMAX[u][0]=calv;
       }
-      
+
     }
-    
+
   }
-  
+
   analogWrite(BUZERPIN,0);
-  
+
 }
+
 
--- mwii_msp_tx/mwii_msp_tx.ino
+++ mwii_msp_tx/mwii_msp_tx.ino
@@ -1,3 +1,12 @@
+/*
+MWII-MSP-TX by Andrej Javorsek
+ November 2012
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ any later version. see <http://www.gnu.org/licenses/>
+ */
+
 #include <avr/pgmspace.h>
 
 #define MSP_SET_RAW_RC 200
@@ -7,18 +16,22 @@
 #define YAWP A3 // 647 512 370        370  512  650 t123
 
 #define RC_REFRESH_RATE 1000/20 //v Hz
-#define LED_REFRESH_RATE 1000/1  //v Hz
+#define LED_REFRESH_RATE 1000/2  //v Hz
 #define LED_ON_OK 9
 #define BUZERPIN 6
 
-uint8_t AUXP[]={10,11,12,5};
+uint8_t AUXP[]={
+  13,12,11,5};
 uint32_t GUT=0 ,T=0, T_LED=0; 
 uint16_t ROLL=1500, PITCH=1500, TH=1000, YAW=1500;
-uint16_t AUX[4]={1000,1000,1000,1000};
-uint16_t SIGN[8]={0,0,0,0};
+uint16_t AUX[4]={
+  1000,1000,1000,1000};
+uint16_t SIGN[8]={
+  0,0,0,0};
 uint8_t CONNECTION_OK=0;
 uint8_t STANJE=0;
-uint8_t ANLOGPINS[]={1,2,3,4};
+uint8_t ANLOGPINS[]={
+  1,2,3,4};
 uint16_t ANLOGMINMAX[4][2];
 
 
@@ -28,7 +41,7 @@ void setup() {
     ANLOGMINMAX[m][0]=384;
     ANLOGMINMAX[m][1]=640;
   }
-  
+
   //stickcal();
   for (int i=0;i<4;i++){
     pinMode(AUXP[i],INPUT);
@@ -42,6 +55,7 @@ void setup() {
 
 void loop() { 
 
+  CONNECTION_OK=1;
   if((GUT-T_LED)>LED_REFRESH_RATE){
     if (CONNECTION_OK){
       STANJE=1;
@@ -53,15 +67,12 @@ void loop() {
     digitalWrite(LED_ON_OK,STANJE);
   }
 
-  //                           (max - vrednost) / (max-min))
-  //                            ((vrednost - (max - (max-min)/2))/((max-min)/2)*1000)+1500
-  //ROLL=((4000*analogRead(ROLLP))/(2*(ANLOGMINMAX[0][1]-ANLOGMINMAX[0][0])))+1499;
   ROLL=(uint16_t) (((double)(ANLOGMINMAX[0][1] - analogRead(ROLLP)) / (ANLOGMINMAX[0][1]-ANLOGMINMAX[0][0]) ) * 1050.0) + 1000;
   PITCH=(uint16_t) (((double)(ANLOGMINMAX[1][1] - analogRead(PITCHP)) / (ANLOGMINMAX[1][1]-ANLOGMINMAX[1][0]) ) * 1050.0) + 1000;
   TH=(uint16_t) (((double)(ANLOGMINMAX[2][1] - analogRead(THP)) / (ANLOGMINMAX[2][1]-ANLOGMINMAX[2][0]) ) * 1050.0) + 1000;
   YAW=(uint16_t) (((double)(ANLOGMINMAX[3][1] - analogRead(YAWP)) / (ANLOGMINMAX[3][1]-ANLOGMINMAX[3][0]) ) * 1050.0) + 1000;
 
- for (int i=0;i<4;i++){
+  for (int i=0;i<4;i++){
     AUX[i]=digitalRead(AUXP[i])*1000+1000;
   }
 
@@ -69,25 +80,18 @@ void loop() {
   SIGN[1]=PITCH;
   SIGN[2]=YAW;
   SIGN[3]=TH;
-  
+
   for(int i=4;i<8;i++){
     SIGN[i]=AUX[i-4];
   }
 
   if((GUT-T)>RC_REFRESH_RATE){
     msp_babel(SIGN);
-     
-     /**(for (int k=0;k<4;k++){
-       Serial.println(SIGN[k],DEC);
-       //Serial.println(ANLOGMINMAX[0][1],DEC);
-       //Serial.println(ANLOGMINMAX[0][0],DEC);
-     }
-     Serial.println(" ");**/
-     
+    //getstatus();
     T=GUT;
   }
-
-
   GUT=millis();
 }
+
+
 
