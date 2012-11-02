8ca70cbbf3692db301c17de19123629fb52b414e
andrejj@gmail.com <andrejj@gmail.com@7106d3dc-2dfb-50bf-8284-94504415b8dd>
2012-11-02 16:27:01 +0000

    Working. Stil without status feedback.

--- trunk/mwii_msp_tx/MSP_BABEL.ino
+++ trunk/mwii_msp_tx/MSP_BABEL.ino
@@ -2,38 +2,38 @@ void msp_babel(uint16_t rx_chs[8]){
   char msp_rx_string[18];
   char tabela[16];
   char checksum=0;
-  
+
   //int msp_babel(int yaw, int th, int roll, int pitch, int aux1, int aux2, int aux3){
-    /*
+  /*
     msp_rx_string[]=velikost_payloada, MSP_SET_RAW_RC, payload[],checksumm
-    */
-    uint8_t msb_pl=0;
-    uint8_t lsb_pl=1;
-    
-    for(int i=0;i<8;i++){
-      tabela[msb_pl]=rx_chs[i] & 0xff;
-      msb_pl=msb_pl+2;
-      
-      tabela[lsb_pl]=rx_chs[i] >> 8;
-      lsb_pl=lsb_pl+2;
-    }
-    
-    msp_rx_string[0]=sizeof(tabela) & 0xff;
-    checksum ^= (sizeof(tabela) & 0xFF);
-    
-    msp_rx_string[1]=MSP_SET_RAW_RC & 0xff;
-    checksum ^= (MSP_SET_RAW_RC & 0xFF);
-    
-    for(int j=0;j<16;j++){
-      msp_rx_string[2+j]=tabela[j] & 0xff;
-      checksum ^= (tabela[j] & 0xff);
-    }
-    
-    msp_rx_string[18]= checksum;
-    
-    Serial.print("$M<"); 
-    for(int u=0;u<19;u++){
-     
-     Serial.write(msp_rx_string[u]);
-    }
+   */
+  uint8_t msb_pl=0;
+  uint8_t lsb_pl=1;
+
+  for(int i=0;i<8;i++){
+    tabela[msb_pl]=rx_chs[i] & 0xff;
+    msb_pl=msb_pl+2;
+
+    tabela[lsb_pl]=rx_chs[i] >> 8;
+    lsb_pl=lsb_pl+2;
+  }
+
+  msp_rx_string[0]=sizeof(tabela) & 0xff;
+  checksum ^= (sizeof(tabela) & 0xFF);
+
+  msp_rx_string[1]=MSP_SET_RAW_RC & 0xff;
+  checksum ^= (MSP_SET_RAW_RC & 0xFF);
+
+  for(int j=0;j<16;j++){
+    msp_rx_string[2+j]=tabela[j] & 0xff;
+    checksum ^= (tabela[j] & 0xff);
+  }
+
+  msp_rx_string[18]= checksum;
+
+  Serial.print("$M<"); 
+  for(int u=0;u<19;u++){
+    Serial.write(msp_rx_string[u]);
+  }
 }
+
--- trunk/mwii_msp_tx/TELEMTRY_RETURN.ino
+++ trunk/mwii_msp_tx/TELEMTRY_RETURN.ino
@@ -1,3 +1,23 @@
 //NOT CURRENTLY USED////
 void getstatus(){
+  char inByte[20];
+  uint8_t i=0;
+  char properResp[]={
+    '$','M','>'      };
+
+  CONNECTION_OK=0;
+  if (Serial.available()) {
+
+    while (Serial.available()) {    
+      inByte[i] = Serial.read();
+      i++;
+    }
+  }
+  if (inByte[0]==properResp[0] && inByte[1]==properResp[1] && inByte[2]==properResp[2]){
+    CONNECTION_OK=1;
+  }
 }
+
+
+
+
