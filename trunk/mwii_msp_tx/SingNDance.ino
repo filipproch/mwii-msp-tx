42a28b368c2f87a7e64fcc01c7a1d851f3112d8d
andrejj@gmail.com <andrejj@gmail.com@7106d3dc-2dfb-50bf-8284-94504415b8dd>
2012-11-04 15:13:07 +0000

    Not air tested! Added some buser signals to AUX switches changes.

---
+++ trunk/mwii_msp_tx/SingNDance.ino
@@ -0,0 +1,49 @@
+//NOT CURRENTLY USED////
+
+void singndance(uint8_t pip){
+ 
+  switch (pip){
+  case 1:
+    PN=NOTE_A5;
+    if ((GUT-TG_SW) > 50){
+      PN=NOTE_A6;
+    }
+    if ((GUT-TG_SW) > 100){
+      PN=NOTE_B7;
+    }
+    if ((GUT-TG_SW) > 150){
+      PN=0;
+    }
+    break;
+    
+  case 2:
+    PN=NOTE_A5;
+    if ((GUT-TG_SW) > 50){
+      PN=NOTE_A6;
+    }
+    if ((GUT-TG_SW) > 100){
+      PN=NOTE_B5;
+    }
+    if ((GUT-TG_SW) > 150){
+      PN=0;
+    }
+    break;
+    
+  default:
+    PN=0;
+    break;
+  }
+
+
+  if (PN != 0){
+    tone(BUZERPIN,PN);
+  }
+  else{
+    noTone(BUZERPIN);
+  }
+
+}
+
+
+
+
